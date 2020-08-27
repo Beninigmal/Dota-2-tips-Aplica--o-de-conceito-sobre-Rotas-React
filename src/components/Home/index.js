a4c500bc46e8353f31983867bf028ac1bab23e2d
Beninigmal <beninigmal@gmail.com>
2020-08-27 16:53:33 -0300

    Correção de bugs nas barras de baixo e do titulo.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../Home/style.css';
 import { Link } from 'react-router-dom';
+import Topbar from '../TopBar';
 
 function Home() {
     return (
         <section>
+            <Topbar />
             <ul className="roles">
                 <Link to="/supp">
                     <li className="kotl">
