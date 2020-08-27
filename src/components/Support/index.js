a5ca111a841081b081eb785e653db7ee740f9770
Beninigmal <beninigmal@gmail.com>
2020-08-27 18:27:06 -0300

    Correção da aplicação quebrada.

--- src/components/Support/index.js
+++ src/components/Support/index.js
@@ -11,7 +11,7 @@ function Support() {
                 </div>
                 <div className="square">
                     <h1>Suporte</h1>
-                    <p>A 'role' suporte, e essencial para o progresso da partida, eles determinam como os seus cores vão conseguir levantar patrimônio o mais rápido para entrar em lutas. Uma das funções essenciais do support é dar visão evitando <a>ganks</a> desnecessários, incomodar os inimigos com suas habilidades mais fortes e o mais importante, prestar atenção em tudo no mapa, pois os cores estaram concentrados demais em fechar seus items.</p>
+                    <p>A 'role' suporte, e essencial para o progresso da partida, eles determinam como os seus cores vão conseguir levantar patrimônio o mais rápido para entrar em lutas. Uma das funções essenciais do support é dar visão evitando ganks desnecessários, incomodar os inimigos com suas habilidades mais fortes e o mais importante, prestar atenção em tudo no mapa, pois os cores estaram concentrados demais em fechar seus items.</p>
                 </div>
 
                 <div className="sup-list">
